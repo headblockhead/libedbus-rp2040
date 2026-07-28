8a98e2fa7a427812887e7abcbfb091f158ab847c
Edward Hesketh <inbox@edwardh.dev>
2026-07-28 18:53:06 +0100

    chore: tidy nix flake

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
   outputs =
-    { nixpkgs, ... }:
+    { self, nixpkgs, ... }:
     let
       pkgsForSystem = system: import nixpkgs { inherit system; };
       supportedSystems = [
@@ -11,36 +11,45 @@
         "aarch64-linux"
       ];
       forEachSystem = nixpkgs.lib.genAttrs supportedSystems;
-      forEachSystemWithPkgs = f: forEachSystem (system: f (pkgsForSystem system));
     in
     {
-      packages = forEachSystemWithPkgs (pkgs: rec {
-        libedbus-rp2040 = pkgs.stdenv.mkDerivation {
-          name = "libedbus-rp2040";
-          src = pkgs.lib.cleanSource ./.;
+      packages = forEachSystem (
+        system:
+        let
+          pkgs = pkgsForSystem system;
+        in
+        rec {
+          libedbus-rp2040 = pkgs.stdenv.mkDerivation {
+            name = "libedbus-rp2040";
+            src = pkgs.lib.cleanSource ./.;
 
-          nativeBuildInputs = with pkgs; [
-            cmake
-            gcc-arm-embedded
-            python313
-          ];
-          cmakeFlags = [
-            "-DCMAKE_C_COMPILER=${pkgs.gcc-arm-embedded}/bin/arm-none-eabi-gcc"
-            "-DCMAKE_CXX_COMPILER=${pkgs.gcc-arm-embedded}/bin/arm-none-eabi-g++"
-          ];
-          env.PICO_SDK_PATH = "${pkgs.pico-sdk}/lib/pico-sdk";
-        };
-        default = libedbus-rp2040;
-      });
-      devShells = forEachSystemWithPkgs (pkgs: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            cmake
-            gcc-arm-embedded
-            python313
-          ];
-          PICO_SDK_PATH = "${pkgs.pico-sdk}/lib/pico-sdk";
-        };
-      });
+            nativeBuildInputs = with pkgs; [
+              cmake
+              gcc-arm-embedded
+              python313
+            ];
+            cmakeFlags = [
+              "-DCMAKE_C_COMPILER=${pkgs.gcc-arm-embedded}/bin/arm-none-eabi-gcc"
+              "-DCMAKE_CXX_COMPILER=${pkgs.gcc-arm-embedded}/bin/arm-none-eabi-g++"
+            ];
+            env.PICO_SDK_PATH = "${pkgs.pico-sdk}/lib/pico-sdk";
+          };
+          default = libedbus-rp2040;
+        }
+      );
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = pkgsForSystem system;
+        in
+        {
+          default = pkgs.mkShell {
+            inputsFrom = [
+              self.packages.${system}.libedbus-rp2040
+            ];
+            PICO_SDK_PATH = "${pkgs.pico-sdk}/lib/pico-sdk";
+          };
+        }
+      );
     };
 }
